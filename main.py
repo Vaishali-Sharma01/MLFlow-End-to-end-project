e0332935754dde329b937a85d58b4557540983b7
Vaishali-Sharma01 <v911991199@gmail.com>
2023-11-05 01:31:29 +0530

    Adding Data Ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from mlflow_project import logger
+from mlflow_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to log testing")
+STAGE_NAME = "Data Ingestion stage"
+try :
+    logger.info(f" Execution of {STAGE_NAME} has started")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"{STAGE_NAME} execution is completed\n\n")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
